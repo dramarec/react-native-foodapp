9778a6d9173f1e43382221170a161a74e3ccffbe
Oleg Dramaretskyi <dramarec@gmail.com>
2021-04-12 14:31:35 +0300

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# food_app
-food_app react-native
---
+++ screens/index.js
@@ -0,0 +1,6 @@
+import Home from './Home';
+
+import Restaurant from './Home';
+import OrderDelivery from './OrderDelivery';
+
+export {Home, Restaurant, OrderDelivery};
